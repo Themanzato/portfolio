78f292f709c7c62b205377460545209454bb7ee0
Manzato <mb13arroyo@gmail.com>
2025-11-28 13:10:57 -0600

    Initial commit: Portfolio personal completo

---
+++ src/data/socials.ts
@@ -0,0 +1,15 @@
+import IconGithub from "../components/icons/github.astro";
+import IconLinkedin from "../components/icons/linkedin.astro";
+
+export const SOCIALS = [
+  {
+    name: "GitHub",
+    url: "https://github.com/Themanzato",
+    icon: IconGithub,
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/mario-alberto-arroyo-utrera-690876299/",
+    icon: IconLinkedin,
+  },
+];
